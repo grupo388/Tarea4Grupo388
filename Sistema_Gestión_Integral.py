5e7161af5232851eb4b2fde606d20cc41189af43
Juan Pérez <juanb2795@gmail.com>
2026-05-11 11:08:06 -0500

    Se realiza la implementación de la clase Reserva - Juan Bautista Pérez Pedrozo

--- Sistema_Gestión_Integral.py
+++ Sistema_Gestión_Integral.py
@@ -52,3 +52,68 @@ class Entidad(ABC):
             print(f"Cliente: {self.__nombre}")
             print(f"Correo: {self.__correo}")
             print(f"Telefono: {self.__telefono}")
+
+class Servicio(ABC):
+    def __init__(self, nombre, costo_base):
+        self.nombre = nombre
+        self.costo_base = costo_base
+    @abstractmethod
+    def calcular_costo(self):
+        pass
+    @abstractmethod
+    def descripcion(self):
+        pass
+
+#Realizo la Reserva de Sala para la Herencia (Andrés Felipe Restrepo Moreno)
+class ReservaSala(Servicio):
+    def calcular_costo(self, horas=1):
+        return self.costo_base * horas
+    def description(self):
+        return "Servicio de reserva de sala"
+    
+#Realizo la clase de alquiler de equipos para el polimorfismo de la herencia (Andrés Felipe Restrepo Moreno)
+class AlquilerEquipos(Servicio):
+    def calcular_costo(self, dias=1):
+        return self.costo_base * dias
+    def descripcion (self):
+        return "Servicio de alquiler de equipos"
+
+#Realizo la clase de asesoría especializada para los servicios especializados (Andrés Felipe Restrepo Moreno)
+class AsesoriaEspecializada(Servicio):
+    def calcular_costo(self, sesiones=1):
+        return self.costo_base * sesiones
+    def descripcion(self):
+        return "Servicio de asesoría especializada"
+
+#Realizo la clase de reserva de salapara el polimorfismo de la herencia.
+class Reserva:
+    def __init__(self, cliente, servicio, duración):
+        if duración <= 0:
+            raise ValidacionError("La duración debe ser mayor a cero.")
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duración = duración
+        self.estado = "Pendiente"
+
+#Realizo el método para la confirmación de la reserva.
+    def confirmar(self):
+        try:
+            costo = self.servicio.calcular_costo(self.duración)
+            if costo <= 0:
+                raise Exception ("El costo no es válido.")
+            self.estado = "confirmada"
+            print("Reserva confirmada")
+            print(f"Costo total: ${costo}")
+#Realizo el método para la cancelación de la reserva.
+        except Exception as e:
+            logging.error(f"Error al confirmar reswerva: {e}")
+            raise ClienteError("No fue posible confirmar la reserva") from e
+    def cancelar(self):
+        self.estado = "Cancelada"
+        print("Resewrva cancelada")
+#Realizo el método para mostrar el estado de la reserva.
+    def mostrar_reserva(self):
+        print("\n==== RESERVA ====")
+        self.cliente.mostrar_info()
+        print(f"Servicio: {self.servicio.descripción()}")
+        print(f"Estado: {self.estado}")
